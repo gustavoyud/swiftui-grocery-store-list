41d85fd8d005fee50514b535a6d1ef263712ec24
Gustavo Yud <gustavo_hy14@hotmail.com>
2025-03-11 23:50:31 -0300

    Initial Commit

---
+++ Shopping List/Shopping_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_ListApp.swift
+//  Shopping List
+//
+//  Created by Gustavo Yud on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
